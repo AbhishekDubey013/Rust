0f2f9e668db77b7c2ca66dfae56c3739b6023984
Abhishek Dubey <abhishekdubey@Abhisheks-MacBook-Pro.local>
2025-08-04 22:12:05 +0530

    Autocache

--- packages/stylus-demo/src/lib.rs
+++ packages/stylus-demo/src/lib.rs
@@ -27,6 +27,7 @@ extern crate alloc;
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
+use stylus_cache_sdk::{is_contract_cacheable};
 
 // Define some persistent storage using the Solidity ABI.
 // `Counter` will be the entrypoint.
@@ -41,28 +42,32 @@ sol_storage! {
 #[public]
 impl Counter {
     /// Gets the number from storage.
-    pub fn number(&self) -> U256 {
+    pub fn number_meet(&self) -> U256 {
         self.number.get()
     }
 
     /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) {
+    pub fn set_number_meet(&mut self, new_number: U256) {
         self.number.set(new_number);
     }
 
     /// Sets a number in storage to a user-specified value.
-    pub fn mul_number(&mut self, new_number: U256) {
+    pub fn mul_number_meet(&mut self, new_number: U256) {
         self.number.set(new_number * self.number.get());
     }
 
     /// Sets a number in storage to a user-specified value.
-    pub fn add_number(&mut self, new_number: U256) {
+    pub fn add_number_meet(&mut self, new_number: U256) {
         self.number.set(new_number + self.number.get());
     }
 
     /// Increments `number` and updates its value in storage.
-    pub fn increment(&mut self) {
+    pub fn increment_meet(&mut self) {
         let number = self.number.get();
-        self.set_number(number + U256::from(1));
+        self.set_number_meet(number + U256::from(1));
+    }
+
+    pub fn is_cacheable(&self) -> bool {
+        is_contract_cacheable()
     }
 }
